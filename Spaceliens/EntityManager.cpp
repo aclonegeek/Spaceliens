5bca18280da5f106d0859b5adaba308909105d86
Randy Taylor <tehgecKozzz@gmail.com>
2016-08-02 17:27:46 -0400

    Implement EntityManager

---
+++ Spaceliens/EntityManager.cpp
@@ -0,0 +1,40 @@
+#include "EntityManager.hpp"
+
+EntityManager::EntityManager(sf::RenderWindow& window)
+	: m_window{ window } {
+}
+
+void EntityManager::add(const std::string& name, std::unique_ptr<Entity> entity) {
+	m_entities.insert(std::make_pair(name, std::move(entity)));
+}
+
+void EntityManager::update(const sf::Time& dt) {
+	for (auto& entity : m_entities) {
+		if (entity.second->isActive()) {
+			entity.second->update(dt);
+		}  else {
+			m_entitiesToRemove.push_back(entity.first);
+			remove();
+		}
+	}
+}
+
+void EntityManager::remove() {
+	for (auto& entity : m_entitiesToRemove) {
+		auto& found = m_entities.find(entity);
+		m_entities.erase(found);
+	}
+
+	m_entitiesToRemove.clear();
+}
+
+void EntityManager::draw() {
+	for (auto& entity : m_entities) {
+		m_window.draw(*entity.second);
+	}
+}
+
+const Entity& EntityManager::getEntity(const std::string& name) const {
+	auto found = m_entities.find(name);
+	return *found->second;
+}
---
+++ Spaceliens/EntityManager.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <map>
+#include <memory>
+#include <vector>
+
+#include "Entity.hpp"
+
+class EntityManager {
+public:
+	EntityManager(sf::RenderWindow& window);
+
+	void add(const std::string& name, std::unique_ptr<Entity> entity);
+	void remove();
+	void update(const sf::Time& dt);
+	void draw();
+
+	const Entity& getEntity(const std::string& name) const;
+private:
+	std::map<std::string, std::unique_ptr<Entity>> m_entities;
+	std::vector<std::string> m_entitiesToRemove;
+	sf::RenderWindow& m_window;
+};
