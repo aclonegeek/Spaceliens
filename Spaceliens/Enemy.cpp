40d000f27b5fcfdfd729ea02312fa418b4f2a2b6
Randy Taylor <tehgecKozzz@gmail.com>
2016-08-07 17:10:40 -0400

    Implement enemy movement

--- Spaceliens/Enemy.cpp
+++ Spaceliens/Enemy.cpp
@@ -1,20 +1,35 @@
 #include "Enemy.hpp"
 
+#include <iostream>
+
 Enemy::Enemy(sf::Vector2f windowSize, sf::Vector2f position)
-	: m_windowSize{ windowSize } {
+	: m_windowSize{ windowSize }
+	, m_moveDown{ false } {
 	active = 1;
 	type = "Enemy";
 	load("enemy.png");
 	setPosition(position);
 }
 
-void Enemy::update(const sf::Time& dt) {
+void Enemy::movement(const sf::Time& dt) {
 	if (getPosition().x < 0) {
-		move(m_speed * dt.asSeconds(), 0.0f);
+		m_speed *= -1;
+		m_moveDown = true;
 	} else if (getPosition().x + getGlobalBounds().width > m_windowSize.x) {
-		move(-m_speed * dt.asSeconds(), 0.0f);
+		m_speed *= -1;
+		m_moveDown = true;
 	}
 
+	if (m_moveDown) {
+		move(m_speed * 1.1f * dt.asSeconds(), 50.0f);
+		m_moveDown = false;
+	} else {
+		move(m_speed * dt.asSeconds(), 0.0f);
+	}
+}
+
+void Enemy::update(const sf::Time& dt) {
+	movement(dt);
 	Entity::update(dt);
 }
 
--- Spaceliens/Enemy.hpp
+++ Spaceliens/Enemy.hpp
@@ -5,10 +5,13 @@ class Enemy : public Entity {
 public:
 	Enemy(sf::Vector2f windowSize, sf::Vector2f position);
 
+	void movement(const sf::Time& dt);
 	void update(const sf::Time& dt);
 	void collision(Entity& entity);
 private:
-	const float m_speed = 200.0f;
+	float m_speed = 200.0f;
 
 	sf::Vector2f m_windowSize;
+
+	bool m_moveDown;
 };
